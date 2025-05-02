5c21fcb9cb9082eea44a3f3ef70d409df47d8cf1
Polarlyst <polarlyst.official@gmail.com>
2025-05-02 16:46:03 +0700

    Update chat.tsx

--- app/components/chat/chat.tsx
+++ app/components/chat/chat.tsx
@@ -366,7 +366,7 @@ export function Chat() {
             }}
           >
             <h1 className="mb-6 text-3xl font-medium tracking-tight">
-              What's on your mind?
+              ให้ฉันช่วยอะไรไหมคะ
             </h1>
           </motion.div>
         ) : (
